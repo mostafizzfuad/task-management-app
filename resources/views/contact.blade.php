490fb2c85ab78763ef9709831f0ff30c05053db6
Mostafizur Rahman <mostafizzfuad@gmail.com>
2025-05-19 23:24:50 +0600

    template mastering

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="container-fluid px-4">
+    <h1 class="mt-4">{{ $title }}</h1>
+    <ol class="breadcrumb mb-4">
+        <li class="breadcrumb-item active">Contact Us</li>
+    </ol>
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header text-center">
+                    <h2>Contact Us</h2>
+                </div>
+                <div class="card-body">
+                    <div class="mb-3">
+                        <label for="" class="form-label">Name</label>
+                        <input type="name" class="form-control" name="" id="" placeholder="Enter Your Name" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="" class="form-label">Email</label>
+                        <input type="email" class="form-control" name="" id="" placeholder="Enter Your Email" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [FrontendController::class, 'index']);
+Route::get('/contact', [FrontendController::class, 'contact']);
